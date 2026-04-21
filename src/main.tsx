553b9a21696d94ecb211d58efd6ca1e857929809
maiconabuquerque <maicaoalbu@gmail.com>
2026-04-21 19:54:15 -0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css';
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  ReactDOM.createRoot(root).render(<App />);
 }
